c15c34f1fc7df4ac41bc71b57d111949b216783d
helen <hhma@alumni.cpp.edu>
2021-01-24 19:41:15 -0500

    first commit

---
+++ src/test/java/guru/springframework/springboot/controller/ExampleControllerTest.java
@@ -0,0 +1,30 @@
+package guru.springframework.springboot.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ExampleControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    public void indexTest() throws Exception{
+        mockMvc.perform(get("/"))
+                .andExpect(jsonPath("$.hello").value("world"));
+     }
+}
